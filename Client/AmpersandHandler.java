a9147c3e10f9cb56145e7f560523e7a1ec8cb98c
Akshay Kokane <30575487+akshaykokane@users.noreply.github.com>
2019-02-23 23:37:33 -0500

    -Updated

--- Client/AmpersandHandler.java
+++ Client/AmpersandHandler.java
@@ -1,10 +1,13 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
@@ -12,43 +15,44 @@ public class AmpersandHandler implements Runnable {
 	
 	String[] commandAndValue;
 	String input = "";		
-	ObjectOutputStream outputStreamAmper = null;
-	ObjectInputStream inputStreamAmper = null;
+	DataOutputStream  outputStreamAmper = null;
+	DataInputStream   inputStreamAmper = null;
 	Socket socket;
 	String commandId;
 	public AmpersandHandler(String input, Socket socket) throws IOException {
 		this.input = input;
 		this.commandAndValue = input.split(" ");
 		this.socket = socket;
-		System.out.println(input);
+		System.out.println("Hello In Constrcutor!");
 		
 	}
 	
 	public void run() {
 		try {
 			
-		outputStreamAmper = new  ObjectOutputStream(socket.getOutputStream());
-		//inputStreamAmper = new ObjectInputStream(socket.getInputStream());
+		outputStreamAmper = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+		inputStreamAmper = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
 		switch(commandAndValue[0]) {
-		
 			case "get&"://client receives file from server
+
+				//Thread.sleep(10000);
+				System.out.println(inputStreamAmper.readChar());
+				//System.out.println("in Get&Y");
 				
-				Thread.sleep(10000);
-				System.out.println(input);
-				System.out.println("in Get&Y");
 				break;
 	
 			case "put&"://client sends file to server
 				//1) Send Command to Server
-				outputStreamAmper.writeObject(input);
+			//	outputStreamAmper.writeObject(input);
 				//2) Receive CommandId from Server
-				commandId = (String) inputStreamAmper.readObject();
+				//commandId = (String) inputStreamAmper.readObject();
 				System.out.println(commandId);
 				break;
 		}
 		
 		
-	} catch (ClassNotFoundException | IOException | InterruptedException e) {
+	} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
